d6d18e937476c2d07fff2f98babc73097e48ab30
ted <ted@warpedpuppy.com>
2020-08-26 09:29:42 -0400

    remove css sizing of canvas

--- src/components/CrystalBall.script.js
+++ src/components/CrystalBall.script.js
@@ -2,10 +2,14 @@ import * as PIXI from 'pixi.js';
 export default {
     init: function() {
         const app = new PIXI.Application({
-            width: 800, height: 600, backgroundColor: 0x110b3f, resolution: window.devicePixelRatio || 1,
+            backgroundColor: 0x110b3f, resolution: window.devicePixelRatio || 1,
         });
-        app.width  = window.innerWidth;
-        app.height = window.innerHeight;
+        // app.width = 600;
+        console.log(app.width, window.innerWidth)
+        // const vpw = window.innerWidth;  // Width of the viewport
+        // const vph = window.innerHeight; // Height of the viewport
+        // app.renderer.resize(vph, vpw);
+
         document.body.appendChild(app.view);
         
         const container = new PIXI.Container();
