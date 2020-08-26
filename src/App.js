c5f7a6379a1448fd710072bce4f7bb81465a6236
ted <ted@warpedpuppy.com>
2020-08-26 08:56:46 -0400

    final draft

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import './App.css';
 import CrystalBall from './components/CrystalBall';
-import solitaire from './applications/games/solitaire/solitaire';
+
 class App extends React.Component {
 
-  componentDidMount () {
-    let s = solitaire;
-    s.init();
-  }
+
   render () {
       return (
       <div className="App">
       <header>
-        <h1>Crystal Ball Test</h1>
+        <h1>Crystal Ball draft</h1>
       </header>
         <main>
           <CrystalBall />
--- src/components/CrystalBall.script.js
+++ src/components/CrystalBall.script.js
@@ -7,13 +7,10 @@ export default {
         document.body.appendChild(app.view);
         
         const container = new PIXI.Container();
-        
         app.stage.addChild(container);
         
-        // Create a new texture
 
         const cloudTexture = PIXI.Texture.from('/bmps/clouds.png');
-
         const clouds = new PIXI.Sprite(cloudTexture);
         clouds.alpha = 0.5;
         clouds.anchor.set(0.5);
@@ -22,35 +19,29 @@ export default {
 
 
         const circleTexture = PIXI.Texture.from('/bmps/circle.png');
-        
         const circle = new PIXI.Sprite(circleTexture);
-        //circle.scale.set(0.5)
         circle.anchor.set(0.5);
         container.addChild(circle);
 
         const coverTexture = PIXI.Texture.from('/bmps/cover.png');
-
         const cover = new PIXI.Sprite(coverTexture);
         cover.anchor.set(0.5);
         cover.x -= 1;
-
         container.addChild(cover);
 
         clouds.mask = circle;
-        // Create a 5x5 grid of bunnies
+ 
 
-        const basicText = new PIXI.Text('one-on-one\nmentoring');
+        const basicText = new PIXI.Text('one-on-one\nmentoring',{fontFamily : 'Arial', fontSize: 50, fill : 0xffffff, align : 'center',fontStyle: 'italic'});
         basicText.anchor.set(0.5);
         container.addChild(basicText);
        
-        console.log(basicText.width)
 
         basicText.mask = circle;
-        // Move container to the center
         container.x = app.screen.width / 2;
         container.y = app.screen.height / 2;
         
-        // Center bunny sprite in local container coordinates
+
         container.pivot.x = container.width / 2;
         container.pivot.y = container.height / 2;
 
@@ -58,14 +49,11 @@ export default {
         let count = 0;
         basicText.filters = [blurFilter1];
         
-        // Listen for animate update
+
         app.ticker.add((delta) => {
             count += 0.005;
             const blurAmount = Math.cos(count);
             blurFilter1.blur = 20 * (blurAmount);
-            // rotate the container!
-            // use delta to create frame-independent transform
-            //container.rotation -= 0.01 * delta;
             let newScale = this.cosWave(1, 0.25, 0.0005);
             clouds.scale.set(Math.abs(newScale))
             clouds.rotation -= 0.001 * newScale;
@@ -73,8 +61,6 @@ export default {
         
     },
     cosWave: function (startPoint, differential, speed) {
-        //place in an onEnterFrame Handler0.0015
-
         var currentDate = new Date();
         return startPoint + (Math.cos(currentDate.getTime() * speed) * differential);
     },
