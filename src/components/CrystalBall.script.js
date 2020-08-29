f632f77048a735a097e4d62341e6feb5c3bf5205
ted <ted@warpedpuppy.com>
2020-08-29 10:26:49 -0400

    changed graphics

--- src/components/CrystalBall.script.js
+++ src/components/CrystalBall.script.js
@@ -1,6 +1,9 @@
 import * as PIXI from 'pixi.js';
+
 export default {
     init: function() {
+
+        
         const app = new PIXI.Application({
             width: 800,
             height: 600,
@@ -8,6 +11,17 @@ export default {
             resolution: window.devicePixelRatio || 1,
         });
        
+        const blurFilter = new PIXI.filters.BlurFilter();
+        let maxBlur = 20,
+            blurAmount = maxBlur, 
+            pauseCounter = 0, 
+            pauseAmount = 2, 
+            fadeQ = -0.25, 
+            alphaQ = 0.01,
+            fadeIn = true,
+            pause = false,
+            fadeOut = true;
+             
         document.body.appendChild(app.view);
         const container = new PIXI.Container();
         app.stage.addChild(container);
@@ -18,7 +32,6 @@ export default {
         clouds.anchor.set(0.5);
         clouds.scale.set(0.5)
         container.addChild(clouds);
-
 
         const circleTexture = PIXI.Texture.from('/bmps/circle.png');
         const circle = new PIXI.Sprite(circleTexture);
@@ -32,30 +45,69 @@ export default {
         container.addChild(cover);
 
         clouds.mask = circle;
- 
 
-        const basicText = new PIXI.Text('one-on-one\nmentoring',{fontFamily : 'Arial', fontSize: 50, fill : 0xffffff, align : 'center',fontStyle: 'italic'});
-        basicText.anchor.set(0.5);
-        container.addChild(basicText);
-       
+        let graphicTexture1 = PIXI.Texture.from(`/bmps/graphic_1.png`);
+        let graphicTexture2 = PIXI.Texture.from(`/bmps/graphic_2.png`);
+        let graphicTexture3 = PIXI.Texture.from(`/bmps/graphic_3.png`);
+        let graphicTexture4 = PIXI.Texture.from(`/bmps/graphic_4.png`);
+        let graphicTexture5 = PIXI.Texture.from(`/bmps/graphic_5.png`);
 
-        basicText.mask = circle;
+        let textures = [graphicTexture1, graphicTexture2, graphicTexture3, graphicTexture4, graphicTexture5];
+        let textureCounter = 0;
+
+        let graphic = new PIXI.Sprite(textures[0]);
+        graphic.anchor.set(0.5);
+        graphic.alpha = 0;
+        container.addChild(graphic);
+        graphic.filters = [blurFilter];
+    
         container.x = app.screen.width / 2;
         container.y = app.screen.height / 2;
-        
-
         container.pivot.x = container.width / 2;
         container.pivot.y = container.height / 2;
-
-        const blurFilter1 = new PIXI.filters.BlurFilter();
-        let count = 0;
-        basicText.filters = [blurFilter1];
         
-
         app.ticker.add((delta) => {
-            count += 0.005;
-            const blurAmount = Math.cos(count);
-            blurFilter1.blur = 20 * (blurAmount);
+
+            if (fadeIn) {
+                //console.log("fadeIn")
+                blurAmount += fadeQ;
+                if(graphic.alpha < 1) graphic.alpha += alphaQ;
+                if(blurAmount < 0) {
+                    fadeIn = false;
+                    fadeOut = false;
+                    pause = true;
+                }
+                
+            } else if (pause) {
+               // console.log("pause")
+                pauseCounter += .01;
+                graphic.alpha = 1;
+                if (pauseCounter > pauseAmount) {
+                    fadeIn = false;
+                    fadeOut = true;
+                    pause = false;
+                }
+            } else if (fadeOut) {
+                //console.log("fade out")
+                blurAmount -= fadeQ;
+                if (graphic.alpha > 0) graphic.alpha -= alphaQ;
+                if (blurAmount > maxBlur) {
+                    
+                    pause = false;
+                    blurAmount = maxBlur;
+                    pauseCounter = 0;
+                    textureCounter ++;
+                    if (textureCounter > textures.length - 1) {
+                        textureCounter = 0;
+                    }
+                    graphic.texture = textures[textureCounter]
+                    fadeIn = true;
+                    fadeOut = false;
+                }
+
+            }
+            
+            blurFilter.blur = (blurAmount);
             let newScale = this.cosWave(1, 0.25, 0.0005);
             clouds.scale.set(Math.abs(newScale))
             clouds.rotation -= 0.001 * newScale;
